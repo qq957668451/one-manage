06ea0a7d4b5da95fa4391059b9bb3079d35c2f57
yk <957668451@qq.com>
2021-10-13 09:49:18 +0800

    commit

---
+++ src/views/pedigree/components/PersonIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <svg width='22px' height='22px' :fill="item.isAlive && item.sex==='man'?'#cddef6':item.isAlive?'#f9c6c6':'#ccc'">
+      <use v-if="item.sex === 'woman'" xlink:href="#icon-mom"></use>
+      <use v-if="item.sex === 'man'" xlink:href="#icon-nanren"></use>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'personIocn',
+  props: {
+    item: {
+      type: Object,
+      default: {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
